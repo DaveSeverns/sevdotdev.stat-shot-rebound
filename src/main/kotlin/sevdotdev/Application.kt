ea7904b24e01512014cc6534489fab2bf551511b
David Severns <dave@Davids-MacBook-Pro.local>
2022-04-18 20:42:31 -0400

    change string format

--- src/main/kotlin/sevdotdev/Application.kt
+++ src/main/kotlin/sevdotdev/Application.kt
@@ -12,12 +12,14 @@ import sevdotdev.plugins.configureRouting
 import sevdotdev.plugins.configureSerialization
 import sevdotdev.repository.StatShotDataRepository
 
+const val DEFAULT_DB_URL = "h2:mem:test;DB_CLOSE_DELAY=-1;"
+const val DEFAULT_DB_DRIVER = "org.h2.Driver"
 fun main(args: Array<String>) {
     val port: Int = System.getenv("PORT")?.toInt() ?: 80
-    val dbUrl: String = System.getenv("DATABASE_URL") ?: ""
-    val dbDriver: String = System.getenv("JDBC_DRIVER")?: ""
+    val dbUrl: String = "jdbc:" + (System.getenv("DATABASE_URL") ?: DEFAULT_DB_URL)
+    val dbDriver: String = System.getenv("JDBC_DRIVER")?: DEFAULT_DB_DRIVER
     embeddedServer(Netty, port = port) {
-        val database = Database.connect(url = dbUrl, driver = dbDriver)
+        val database = Database.connect(dbUrl, driver = dbDriver)
         val statsDao = StatsDao(database)
 
         val repository = StatShotDataRepository(
